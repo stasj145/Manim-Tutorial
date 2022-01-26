004c8cdcaf7074c372c585a3731841dd376d0133
Jonas Bevernis <jonas.bevernis@hochschule-stralsund.de>
2022-01-26 12:55:56 +0100

    test

--- WorkDirJonas/test.py
+++ WorkDirJonas/test.py
@@ -1,8 +1,23 @@
 from manim import *
 
 
-class CreateCircle(Scene):
+class test(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+        
+        arrow = Arrow(start=RIGHT, end=LEFT, color=GREEN)
+        arrow.next_to(circle, LEFT)
+
+        dot = Dot()
+        dot.next_to(circle, RIGHT, buff=2)
+        
+        dotOnCircle = dot.copy()
+        dotOnCircle.move_to(circle)
+        dotOnCircle.shift(RIGHT)
+
+        self.add(dot)
+        self.play(Create(circle))
+        self.play(Create(arrow))
+        self.play(Transform(dot, dotOnCircle))
+        self.play(MoveAlongPath(dot,circle))
